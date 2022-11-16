a0d80804d852fa75a635742c30fdefb2e52fd31e
Vladimir <user@MAK.local>
2022-11-16 19:44:29 +0700

    добавлен ввод числа с консоли

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿/*Задайте значение N. Напишите программу, которая выведет 
+все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.*/
+
+Console.WriteLine("Введите число N");
+int num = int.Parse(Console.ReadLine()!);
+
+string PrintNumbers(int num)
+{
+    if (num == 1) 
+        return num.ToString();
+
+    else
+    {
+        Console.Write ((num) + ",");
+        num = (num-1);
+        return PrintNumbers (num);
+         
+    }
+}
+
+Console.WriteLine(PrintNumbers(num));
